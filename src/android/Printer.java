bd306fc776aeb8ba941b4f8b310c62195f165cbe
Roberson Sathler <robersonsn@gmail.com>
2019-06-25 10:26:32 -0300

    adicionada referencia java da biblioteca de impressao da cielo-lio

--- src/android/Printer.java
+++ src/android/Printer.java
@@ -24,13 +24,31 @@ package de.appplant.cordova.plugin.printer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.webkit.WebView;
+import android.util.Log;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
+
+import cielo.printer.client.PrinterAttributes;
+import cielo.sdk.order.PrinterListener;
+import cielo.sdk.printer.PrinterManager;
 
 /**
  * Plugin to print HTML documents. Therefore it creates an invisible web view
@@ -55,6 +73,58 @@ public final class Printer extends CordovaPlugin
      *
      * @return         Whether the action was valid.
      */
+
+    private Builder createDialog(CordovaInterface cordova) {
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+            return new Builder(cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+        } else {
+            return new Builder(cordova.getActivity());
+        }
+    }
+
+    private void changeTextDirection(Builder dlg){
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        dlg.create();
+        AlertDialog dialog =  dlg.show();
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            TextView messageview = (TextView)dialog.findViewById(android.R.id.message);
+            messageview.setTextDirection(android.view.View.TEXT_DIRECTION_LOCALE);
+        }
+    }
+
+    public synchronized void alert(final String message, final String title, final String buttonLabel, final CallbackContext callbackContext) {
+    	final CordovaInterface cordova = this.cordova;
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+
+                Builder dlg = createDialog(cordova); // new AlertDialog.Builder(cordova.getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT);
+                dlg.setMessage(message);
+                dlg.setTitle(title);
+                dlg.setCancelable(true);
+                dlg.setPositiveButton(buttonLabel,
+                        new AlertDialog.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, 0));
+                            }
+                        });
+                dlg.setOnCancelListener(new AlertDialog.OnCancelListener() {
+                    public void onCancel(DialogInterface dialog)
+                    {
+                        dialog.dismiss();
+                        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, 0));
+                    }
+                });
+
+                changeTextDirection(dlg);
+            };
+        };
+        this.cordova.getActivity().runOnUiThread(runnable);
+    }
+
+
     @Override
     public boolean execute (String action, JSONArray args,
                             CallbackContext callback)
@@ -71,7 +141,18 @@ public final class Printer extends CordovaPlugin
         }
         else if (action.equalsIgnoreCase("print"))
         {
-            print(args.optString(0), args.optJSONObject(1), callback);
+
+            try {
+
+                print(args.optString(0), args.optJSONObject(1), callback);
+                valid = true;
+
+            } catch (Exception e) {
+
+                callback.error(e.getMessage());
+                valid = false;
+            }
+
         }
         else {
             valid = false;
@@ -121,14 +202,36 @@ public final class Printer extends CordovaPlugin
      * @param settings Additional settings how to render the content.
      * @param callback The plugin function to invoke with the result.
      */
-    private void print (@Nullable String content, JSONObject settings,
-                        CallbackContext callback)
+    private void print (@Nullable String content, JSONObject settings, CallbackContext callback) throws Exception
     {
-        cordova.getThreadPool().execute(() -> {
-            PrintManager pm = new PrintManager(cordova.getContext());
-            WebView view    = (WebView) webView.getView();
 
-            pm.print(content, settings, view, (boolean completed) -> sendPluginResult(callback, completed));
+        cordova.getThreadPool().execute(() -> {
+            PrinterManager printerManager = new PrinterManager(cordova.getContext());
+
+            PrinterListener printerListener = new PrinterListener() {
+                @Override public void onPrintSuccess() {
+                    callback.success();
+                }
+
+                @Override public void onError(@Nullable Throwable e) {
+                    callback.error(e.getMessage());
+                    
+                }
+
+                @Override public void onWithoutPaper() {
+                    callback.error("Sem papel");
+                }
+            };
+
+            HashMap<String, Integer> alignLeft =  new HashMap<>();
+            alignLeft.put(PrinterAttributes.KEY_ALIGN, PrinterAttributes.VAL_ALIGN_LEFT);
+            alignLeft.put(PrinterAttributes.KEY_MARGIN_TOP, 50);
+            alignLeft.put(PrinterAttributes.KEY_MARGIN_BOTTOM, 50);
+            alignLeft.put(PrinterAttributes.KEY_TYPEFACE, 0);
+            alignLeft.put(PrinterAttributes.KEY_TEXT_SIZE, 20);
+
+            printerManager.printText(content, alignLeft, printerListener);            
+
         });
     }
 
